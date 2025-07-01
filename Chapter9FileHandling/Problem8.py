68b42f3f9aaafba7bc055af34dff83242a28e193
Rohan Mihra <rohanmishra023@gmail.com>
2025-07-01 15:22:18 +0530

    Question 8

---
+++ Chapter9FileHandling/Problem8.py
@@ -0,0 +1,10 @@
+#write a program to make a copy of text file "this.txt"
+# Program to make a copy of text file "this.txt"
+
+# Open the source file in read mode
+with open(r"Chapter9FileHandling\this.txt", "r") as f:
+    # Open the destination file in write mode
+    with open(r"Chapter9FileHandling\copy.txt", "w") as f1:
+        # Copy line by line
+        for line in f:
+            f1.write(line)
