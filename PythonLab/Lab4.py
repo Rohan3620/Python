a5bd95c3f588f75c1398bed2c11360a3024dbdab
Rohan Mishra <rohanmishra023@gmail.com>
2025-09-26 23:00:38 +0530

    update

---
+++ PythonLab/Lab4.py
@@ -0,0 +1,18 @@
+str = "Rohan Mishra"
+
+print("First six chars       : ", str[0:6])
+print("First six chars       : ", str[:6])
+print("First six chars       : ", str[0:6:1])
+print("Second word           : ", str[6:])
+print("From index 2 to end   : ", str[2:])
+print("Last three chars      : ", str[-3:])
+print("Every 2nd char        : ", str[0::2])
+print("Reverse string        : ", str[::-1])
+print("Index 1 to 6 step 2   : ", str[1:6:2])
+print("Middle part (4 to 9)  : ", str[4:10])
+print("Without first char    : ", str[1:])
+print("Without last char     : ", str[:-1])
+print("Only first char       : ", str[0])
+print("Only last char        : ", str[-1])
+
+
